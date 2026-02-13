d39a9e602d7fd0a667a212e6fa921a27acf990dc
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2026-02-12 21:20:36 -0300

    adicionando Ideia de Id númerico para Partidas

--- Games/LeagueOfLegends/Classes/Match.cs
+++ Games/LeagueOfLegends/Classes/Match.cs
@@ -7,11 +7,26 @@ namespace PEJC.Games.LeagueOfLegends.Classes
 {
     public class Match
     {
+        private static int id = 0;
+
         int Kills {get;set;}
         int Deaths {get;set;}
         int Assists {get;set;}
         int PdlGained {get;set;}
         int MaestryGained {get;set;}
         string? LanePlayed {get;set;}
+        Champion Champion {get;set;}
+
+        public Match()
+        {
+            AtribuirId();
+        }
+
+
+            public void AtribuirId()
+        {
+            id++;
+            Console.WriteLine($"Parabens, essa é a partida de número {id}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿using PEJC.Games.LeagueOfLegends.Classes;
+
+Match partida01 = new Match();
+
+Match partida02 = new Match();
+
+Match partida03 = new Match();
