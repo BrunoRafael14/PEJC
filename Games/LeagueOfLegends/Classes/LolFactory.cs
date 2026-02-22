3fb4e02ffd372d5190488b10f86b9ab93037a167
BrunoRafael14 <brunorafaelfilho10@gmail.com>
2026-02-22 20:24:09 -0300

    Fazendo métodos para instanciar objetos das classes Principais

--- Games/LeagueOfLegends/Classes/LolFactory.cs
+++ Games/LeagueOfLegends/Classes/LolFactory.cs
@@ -12,7 +12,7 @@ namespace PEJC.Games.LeagueOfLegends.Classes
         {
             Console.WriteLine("Digite o nome do Campeão: ");
             string? championName= Console.ReadLine();
-            Console.WriteLine("Digite a quantidade Total de Maestria com o Campeoão: ");
+            Console.WriteLine("Digite a quantidade Total de Maestria com o Campeão: ");
             double championMaestry = Convert.ToDouble(Console.ReadLine());
 
             Champion champion = new Champion();
@@ -21,6 +21,35 @@ namespace PEJC.Games.LeagueOfLegends.Classes
 
 
             return champion;
+        }
+
+        public static Match CreateMatch()
+        {
+            Console.WriteLine("Informe quantidade de Kills: ");
+            int killsMatch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe quantidade de Mortes: ");
+            int deathsMatch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe quantidade de Assistências: ");
+            int assistsMatch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe quantidade de PDL Ganho: ");
+            int pdlGainedMatch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe quantidade de Maestria Ganha: ");
+            int maestryGainedMatch = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Informe a Lane Jogada (Toplane, Midlane, Jungle, Atirador, Suporte): ");
+            string? LanePlayedMath = Console.ReadLine();
+            Console.WriteLine("Informe Nome do Campeão utilizado: ");
+            string? ChampionMath = Console.ReadLine();
+
+            Match match = new Match();
+            match.Kills = killsMatch;
+            match.Deaths = deathsMatch;
+            match.Assists = assistsMatch;
+            match.PdlGained = pdlGainedMatch;
+            match.MaestryGained = maestryGainedMatch;
+            match.LanePlayed = LanePlayedMath;
+            // Falta colocar o champion, ainda vou ver como consigo adicionar adicionar o campeão sem instanciar a outra classe Champion
+
+            return match;
         }
     }
 }
---
+++ Games/LeagueOfLegends/Data/JsonService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PEJC.Games.LeagueOfLegends.Classes;
+
+namespace PEJC.Games.LeagueOfLegends.Data
+{
+    public class JsonService
+    {
+        public static void SaveChampionData()
+        {
+            
+        }
+    }
+}
